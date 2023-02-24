fb6ba80cac81427ce0c0bd5b34cc672417688abc
Nikita Istomin <nikita.istomin54@gmail.com>
2023-02-23 23:48:44 -0600

    Finished assignment

---
+++ README.txt
@@ -0,0 +1,65 @@
+Submission includes:
+    - main.py
+    - main.ipynb
+
+Part 1 & 2 are written together in the same file.
+Both files contain the same code. One is a pure python file, the other is a Jupyter notebook (which I find easier to run).
+SKLearn SGDRegressor was used for the second part of the assignment.
+
+            #################
+            |  How to run:  |
+            #################
+
+Option 1 - Run main.ipynb file in Google Colab
+    1. Go to Google Colab.
+    2. Import main.ipynb into an empty notebook.
+    3. Run all cells.
+    4. Output and graphs are inline with code.
+
+
+
+Option 2 - Run main.ipynb file with VSCode
+    1. Install Python ( Tested on Python 3.11.2 )
+    
+    2. Install required libraries: 
+    numpy:
+        pip install numpy
+    pandas:
+        pip install pandas
+    matplotlib:
+        python -m pip install -U pip
+        python -m pip install -U matplotlib
+    sklearn:
+        pip install -U scikit-learn
+
+    3. Install VSCode.
+
+    4. Install the official Python extension for VSCode.
+
+    5. Open main.ipynb with VSCode.
+
+    6. Run all cells.
+
+
+
+Option 3 - Run main.py file in a command line
+    1. Install Python ( Tested on Python 3.11.2 )
+    
+    2. Install required libraries: 
+    numpy:
+        pip install numpy
+    pandas:
+        pip install pandas
+    matplotlib:
+        python -m pip install -U pip
+        python -m pip install -U matplotlib
+    sklearn:
+        pip install -U scikit-learn
+
+    3. Navigate to the folder with main.py file in the terminal
+
+    4. Run the following command ("python" must be associated with the right version)
+        python main.py
+
+    5. You will see text output in the console and you will get a graph popup. Close a graph window to see the next graph (3 total graphs).
+
--- scr/main.py
+++ scr/main.py
@@ -1,3 +1,7 @@
+"""Nikita Istomin - CS4375 - Assignment 1 - Gradient Descent Algorithm
+    Date: February 23rd, 2023
+"""
+
 import numpy as np
 import pandas as pd
 import math
@@ -13,10 +17,9 @@ plt.style.use(['ggplot'])
 
 
 class GradientDescent():
-    def __init__(self, learning_rate=0.01, iterations=1000, tolerance=1e-06):
+    def __init__(self, learning_rate=0.01, iterations=1000):
         self.learning_rate: float = learning_rate
         self.iterations: int = iterations
-        self.tolerance: float = tolerance
         self.dataset: pd.DataFrame = None
         self.X_train: np.ndarray = None
         self.X_test: np.ndarray = None
@@ -36,6 +39,14 @@ class GradientDescent():
         self.dataset = pd.read_csv(url)
 
     def fit(self):
+        """Fit the model to the dataset using gradient descent
+
+        Args:
+            None
+
+        Returns:
+            list: list of costs for each iteration
+        """
         # check if dataset has been preprocessed
         if self.X_train is None:
             # preprocess the dataset with all the columns assuming the last column is the Y
@@ -60,6 +71,10 @@ class GradientDescent():
         W = generateWVector(X)
         costs = []
         n = len(X)
+        
+        # print(f'X Shape: {X.shape}')
+        # print(f'W Shape: {W.shape}')
+        # print(f'Y Shape: {Y.shape}')
 
         # gradient descent algorithm
         for i in range(self.iterations):
@@ -113,7 +128,7 @@ class GradientDescent():
         self.Y_train = np.array(self.Y_train)
         self.Y_test = np.array(self.Y_test)
 
-    def rename_dataset_columns(self, columns: dict[str: str]):
+    def rename_dataset_columns(self, columns):
         """Rename the dataset columns
 
         Args:
@@ -126,13 +141,26 @@ class GradientDescent():
 
 class Analysis():
 
-    def plot_data(self, labels: dict[str: str], X: np.ndarray, Y: np.ndarray, W=None, label="") -> None:
-        # plot the features against the label
+    def plot_data(self, labels, X: np.ndarray, Y: np.ndarray, W=None, label="") -> None:
+        """Plot the data in a grid by feature
+        
+        Args:
+            labels (dict[str: str]): dictionary of labels for the X and Y axis
+            X (np.ndarray): X value matrix
+            Y (np.ndarray): Y value vector
+            W (np.ndarray, optional): Weights vector. Defaults to None.
+            label (str, optional): Title of the plot. Defaults to empty string.
+
+        Returns:
+            None
+        """
         fig = plt.figure(figsize=(30,20))
         fig.suptitle(label, fontsize=30)
 
-        plot_w = 2
+        plot_w = 1 if len(labels) <= 2 else 2
         plot_h = math.ceil(len(labels) / plot_w)
+        font = 22 if plot_w == 1 else 16
+        plt.rcParams.update({'font.size': font})
         for i, (x, y) in enumerate(labels.items()):
             plt.subplot(plot_h, plot_w, i+1)
             plt.plot(X[:, i], Y, 'b.')
@@ -140,11 +168,20 @@ class Analysis():
                 X_plot = X[:, i]
                 Y_plot = np.concatenate((np.ones((len(X), 1)), X), axis=1).dot(W)
                 plt.plot(X_plot, Y_plot, 'r.')
-            plt.ylabel(y, fontsize=20)
-            plt.xlabel(x, fontsize=20)
+            plt.ylabel(y, fontsize=font+2)
+            plt.xlabel(x, fontsize=font+2)
         plt.show()
 
     def info(self, Y_real: np.ndarray, Y_pred: np.ndarray):
+        """Print the MSE, R2 and Explained Variance scores
+        
+        Args:
+            Y_real (np.ndarray): Real Y values
+            Y_pred (np.ndarray): Predicted Y values
+
+        Returns:
+            None
+        """
         mse = mean_squared_error(Y_real, Y_pred)
         r2 = r2_score(Y_real, Y_pred)
         variance = explained_variance_score(Y_real, Y_pred)
@@ -158,9 +195,10 @@ class Analysis():
 
 
 
+
 if __name__ == "__main__":
     # initialize the gradient descent class
-    gd = GradientDescent()
+    gd = GradientDescent(iterations=3000, learning_rate=0.001)
     print("Fetching dataset... ", end="")
     gd.fetch_dataset('https://raw.githubusercontent.com/NorthPhoenix/ML-Gradual_Descent_Algorithm/main/dataset/Tetuan%20City%20power%20consumption.csv')
     print("Done")
@@ -169,27 +207,34 @@ if __name__ == "__main__":
     print("Preprocessing dataset... ", end="")
     gd.rename_dataset_columns(
         {'Zone 1 Power Consumption': 'Power', 'Temperature': 'Temp', 'Wind Speed': 'Wind', 'general diffuse flows': 'Gen_Dif_Flow', 'diffuse flows': 'Dif_Flow'})
-    features = ['Temp', 'Humidity', 'Gen_Dif_Flow', 'Dif_Flow']
+    features = ['Temp', 'Humidity', 'Gen_Dif_Flow']
     label = 'Power'
     gd.preprocess(features, label)
     print("Done")
 
     # train the model
-    print("Training model... ", end="")
+    print("Training my model... ", end="")
     costs = gd.fit()
     print("Done")
 
     # predict the train set
+    print("Predicting the training set using my model... ", end="")
     Y_train_pred = gd.predict(gd.X_train, gd.W)
     Y_test_pred = gd.predict(gd.X_test, gd.W)
+    print("Done")
 
     # initialize the SGD regressor
-    sgd_regressor = SGDRegressor()
+    print("Training the SKLearn model... ", end="")
+    sgd_regressor = SGDRegressor(max_iter=3000)
     # train the model
     sgd_regressor.fit(gd.X_train, gd.Y_train)
+    print("Done")
+
     # predict the test set
+    print("Predicting the testing set using the SKLearn model... ", end="")
     SK_Y_train_pred = sgd_regressor.predict(gd.X_train)
     SK_Y_test_pred = sgd_regressor.predict(gd.X_test)
+    print("Done")
 
     analysis = Analysis()
 
@@ -197,11 +242,13 @@ if __name__ == "__main__":
     analysis.info(gd.Y_train, Y_train_pred)
     print("My model's performance for test set")
     analysis.info(gd.Y_test, Y_test_pred)
+    print()
     print("SKLearn model's performance for training set")
     analysis.info(gd.Y_train, SK_Y_train_pred)
     print("SKLearn model's performance for test set")
     analysis.info(gd.Y_test, SK_Y_test_pred)
 
-analysis.plot_data(dict.fromkeys(features, label), gd.X_test, gd.Y_test, W=gd.W, label='My model on the testing set')
-analysis.plot_data(dict.fromkeys(features, label), gd.X_test, gd.Y_test, W=np.hstack((sgd_regressor.intercept_, sgd_regressor.coef_)), label='SKLearn model on the testing set')
+    analysis.plot_data({'Iterations': 'Cost'}, np.arange(len(costs)).reshape(len(costs), 1), costs, label='Cost vs Iterations of my model')
+    analysis.plot_data(dict.fromkeys(features, label), gd.X_test, gd.Y_test, W=gd.W, label='My model against the testing set')
+    analysis.plot_data(dict.fromkeys(features, label), gd.X_test, gd.Y_test, W=np.hstack((sgd_regressor.intercept_, sgd_regressor.coef_)), label='SKLearn model on the testing set')
 
